177752845418b2cfd066e496fe9528a8b59981cf
Jon Tan <joanthan.tan83@gmail.com>
2015-04-03 10:39:15 -1000

    adds data binding of controller and layout

--- src/modules/app/listing/listingController.js
+++ src/modules/app/listing/listingController.js
@@ -3,4 +3,9 @@
 module.exports = /*@ngInject*/
   function listingController($scope) {
     $scope.welcome = 'This is the Listing';
+
+    $scope.info = {
+      title : "this is title",
+      description : "this is description"
+    };
   };
