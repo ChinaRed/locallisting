90231347a979a87545d69bd047a091c71463e741
Jon Tan <joanthan.tan83@gmail.com>
2015-04-03 10:56:59 -1000

    adds server to project

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(PORT, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
